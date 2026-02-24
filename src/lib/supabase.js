6fab8a85dd97fd8974666f2090e16a6d27d4daa7
fajaruddin2004 <udinfajar821@gmail.com>
2026-02-24 22:25:25 +0800

    Cek kunci

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -4,4 +4,5 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 // KATA KUNCI 'export const supabase' INI YANG BIKIN ERROR KALAU HILANG
+console.log("CEK KUNCI VERCEL:", process.env.NEXT_PUBLIC_SUPABASE_URL ? "AMAN BOS!" : "KOSONG/HILANG!");
 export const supabase = createClient(supabaseUrl, supabaseKey);
